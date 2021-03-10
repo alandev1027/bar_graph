0f68dfae52a57c9e9143404bab7dd17d75eecc28
alandev1027 <alandev1027@gmail.com>
2021-03-10 03:39:34 -0600

    updated getNodeLists

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,27 +1,27 @@
 export const getNodeLists = (nodeMap) => {
-  const nodekeys = Object.keys(nodeMap);
+  let nodekeys = Object.keys(nodeMap);
 
   if (!nodekeys.length) return [];
 
   let nodeLists = [[nodekeys[0]]];
-  let isLoop = true;
-  while (isLoop) {
+  while (nodekeys.length) {
     let newList = [];
-    nodeLists.forEach((nodeList) => {
-      const lastNode = nodeList[nodeList.length - 1];
-      if (nodeMap[lastNode].adjList.length) {
-        nodeMap[lastNode].adjList.forEach((node) =>
+    let nextNodeKey;
+    for (let index = 0; index < nodeLists.length; index++) {
+      let nodeList = nodeLists[index];
+      nextNodeKey = nodeList[nodeList.length - 1];
+      if (nodeMap[nextNodeKey].adjList.length) {
+        nodeMap[nextNodeKey].adjList.forEach((node) =>
           newList.push([...nodeList, node])
         );
       } else {
         newList.push(nodeList);
       }
-    });
+      if (nodekeys.includes(nextNodeKey))
+        nodekeys.splice(nodekeys.indexOf(nextNodeKey), 1);
+    }
+
     nodeLists = newList;
-    const filterList = nodeLists.filter(
-      (nodeList) => nodeMap[nodeList[nodeList.length - 1]].adjList.length
-    );
-    isLoop = filterList.length;
   }
   return nodeLists;
 };
